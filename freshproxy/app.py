55e244c5aac177f0beaf734ee23491a2be43ece5
hstct <code@elmc.at>
2025-01-05 13:37:43 +0100

    refactor: typesafety, additional logging for app

--- freshproxy/app.py
+++ freshproxy/app.py
@@ -3,19 +3,25 @@ import logging
 from flask import Flask
 from flask_cors import CORS
 
-from freshproxy.config import ALLOWED_ORIGINS, DEBUG
+from freshproxy.config import ALLOWED_ORIGINS, LOG_FORMAT, LOG_LEVEL
 from freshproxy.proxy_routes import proxy_bp
 
 
-def create_app():
+def create_app() -> Flask:
     """
     Application factory that creates and configures the Flask app.
+
+    Returns:
+        Flask: Configured Flask application instance.
     """
     app = Flask(__name__)
 
-    logging.basicConfig(level=logging.INFO if not DEBUG else logging.DEBUG)
+    logging.basicConfig(level=LOG_LEVEL, format=LOG_FORMAT)
+    logger = logging.getLogger(__name__)
+    logger.debug("Logging is configured")
 
-    app.register_blueprint(proxy_bp, url_prefix="")
+    app.register_blueprint(proxy_bp)
+    logger.debug("Blueprint 'proxy_bp' registered.")
 
     CORS(app, resources={r"/": {"origins": ALLOWED_ORIGINS}})
 
