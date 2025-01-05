28cb81f710e53378f12eaad3ab6d9ea879a84afd
hstct <code@elmc.at>
2025-01-05 13:33:43 +0100

    feat: add request timeout, log config and types to config

--- freshproxy/config.py
+++ freshproxy/config.py
@@ -1,24 +1,38 @@
 import os
 import logging
 
+from typing import List, Dict
 from dotenv import load_dotenv
+
+logger = logging.getLogger(__name__)
 
 # Load environment variables from .env file (if present)
 load_dotenv()
 
-AUTH_TOKEN = os.getenv("FRESHRSS_API_TOKEN", "")
-BASE_URL = os.getenv("FRESHRSS_BASE_URL", "").rstrip("/")
-ALLOWED_ORIGINS = os.getenv("FRESHPROXY_ALLOWED_ORIGINS", "")
-ALLOWED_ENDPOINTS = {"subscriptions": "subscription/list", "feed": "stream/contents/feed"}
+# Retrieve environment variables
+AUTH_TOKEN: str = os.getenv("FRESHRSS_API_TOKEN", "")
+BASE_URL: str = os.getenv("FRESHRSS_BASE_URL", "").rstrip("/")
+REQUEST_TIMEOUT: int = int(os.getenv("FRESHPROXY_REQUEST_TIMEOUT", 10))
+ALLOWED_ORIGINS_ENV: str = os.getenv("FRESHPROXY_ALLOWED_ORIGINS", "")
+ALLOWED_ENDPOINTS: Dict[str, str] = {"subscriptions": "subscription/list", "feed": "stream/contents/feed"}
 
-ALLOWED_ORIGINS = [origin.strip() for origin in ALLOWED_ORIGINS.split(",")]
+# Split, strip, and filter out empty strings
+ALLOWED_ORIGINS: List[str] = [
+    origin.strip() for origin in ALLOWED_ORIGINS_ENV.split(",") if origin.strip()
+]
 
-DEBUG = os.getenv("FRESHPROXY_DEBUG", "False").lower() == "true"
-HOST = os.getenv("FRESHPROXY_HOST", "0.0.0.0")
-PORT = int(os.getenv("FRESHPROXY_PORT", 8000))
+# Server config
+DEBUG: bool = os.getenv("FRESHPROXY_DEBUG", "False").lower() == "true"
+HOST: str = os.getenv("FRESHPROXY_HOST", "0.0.0.0")
+PORT: int = int(os.getenv("FRESHPROXY_PORT", 8000))
 
+# Logging config
+LOG_FORMAT: str = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+LOG_LEVEL: int = logging.DEBUG if DEBUG else logging.INFO
+
+# Warning if essential environment variable is missing
 if not AUTH_TOKEN or not BASE_URL:
-    logging.warning(
+    logger.warning(
         "Either FRESHRSS_API_TOKEN or FRESHRSS_BASE_URL is missing. "
         "Proxy may not function correctly."
     )
